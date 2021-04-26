fec35b048d60dffea6d0740448bf94a32cc119e5
Geovanne Moro <geovannemoro@hotmail.com>
2021-04-26 17:07:54 -0300

    Add - Tests Create Statement Use Case

--- src/modules/statements/repositories/in-memory/inMemoryTransfersRepository.ts
+++ src/modules/statements/repositories/in-memory/inMemoryTransfersRepository.ts
@@ -2,9 +2,9 @@ import { v4 as uuid } from "uuid";
 
 import { Transfer } from "../../entities/Transfer";
 import { ICreateTransferDTO } from "../../useCases/createTransfer/ICreateTransferDTO";
-import { ITransferRepository } from "../ITransferRepository";
+import { ITransfersRepository } from "../ITransfersRepository";
 
-export class InMemoryTransfersRepository implements ITransferRepository {
+export class InMemoryTransfersRepository implements ITransfersRepository {
   private transfers: Transfer[] = [];
 
   async create(data: ICreateTransferDTO): Promise<Transfer> {
